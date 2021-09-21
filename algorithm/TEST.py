f40931918196afe350b3d1ff847ea257afd59aa8
jiac <463045792@qq.com>
2021-09-21 11:25:05 +0800

    算法部分目录调整

--- algorithm/TEST.py
+++ algorithm/TEST.py
@@ -2,6 +2,7 @@
 
 
 if __name__ == '__main__':
-    item = [1, 2, 3]
-    item.extend(([4,5,65]))
+    item = ["i", "have", "an", "apple", "he", "have", "a"]
+    item = sorted(item)
+    print(item[0] < item[5])
     print(item)
